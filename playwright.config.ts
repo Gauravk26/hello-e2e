0b6495dc3197e0f5a860698d7b5e9f6137410fb0
Kumar, Gaurav <Gaurav.Kumar+EWIT@eurowings.com>
2025-09-07 18:06:15 +0200

    w

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,9 +11,13 @@ export default defineConfig({
   use: {
     trace: 'on-first-retry',
   },
+  use: {
+    baseURL: 'http://localhost:5173',
+    trace: 'on-first-retry',
+  },
   webServer: {
-    command: 'npm run preview',
-    port: 'http://127.0.0.1:5173',
+    command: 'npm run dev',
+    url: 'http://127.0.0.1:5173',
     reuseExistingServer: !process.env.CI,
     timeout: 60_000
   },
